bd49b0b63c729caac12e78b4dd4f700bef8d8b92
Gaellokossou <lxolalikokouguel@gmail.com>
2025-04-18 19:59:45 +0000

    ..

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('accueil');
+
+Route::get('/dashboard', function () {
+    return view('layout.dashboard');
+})->name('dashboard');
